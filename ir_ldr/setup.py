46ab118fc4148d1c1fa3482ba58dd22b3c75f7ce
Mingjie Jian <ssaajianmingjie@gmail.com>
2019-02-27 19:14:58 +0900

    First commit

---
+++ ir_ldr/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ruler',
+      version='0.1',
+      description='The ruler for spectra',
+      url='',
+      author='Mingjie Jian',
+      author_email='ssaajianmingjie@gmail.com',
+      license='MIT',
+      packages=['ruler'],
+      zip_safe=False)
